61fbc94489cdd87e86104a896553c1838aaec9db
d4-5 <111686428+d4-5@users.noreply.github.com>
2026-06-04 15:12:12 +0000

    task3

---
+++ storage.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Project = var.project_id
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
--- variables.tf
+++ variables.tf
@@ -1,21 +1,5 @@
-variable "ssh_key" {
-  description = "Provides custom public SSH key"
-  type        = string
-}
-
-variable "aws_keypair_name" {
-  description = "Name of the AWS EC2 key pair used for SSH access"
-  type        = string
-}
-
-variable "aws_instance_name" {
-  description = "Name of the EC2 instance to create"
-  type        = string
-}
-
-variable "project" {
-  description = "Project name"
-  default     = "epam-tf-lab"
+variable "aws_region" {
+  description = "AWS region where resources will be created"
   type        = string
 }
 
@@ -24,30 +8,7 @@ variable "project_id" {
   type        = string
 }
 
-variable "aws_region" {
-  description = "AWS region where resources will be created"
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name"
   type        = string
-}
-
-variable "ami_id" {
-  description = "AMI ID for the EC2 instance."
-  type        = string
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
-}
-
-variable "aws_security_group_name" {
-  description = "Name tag of the existing security group."
-  type        = string
-}
-
-locals {
-  common_tags = {
-    Project = var.project
-    ID      = var.project_id
-  }
 }
